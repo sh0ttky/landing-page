aa88f52258e238b6e3d94b6942340119409dab60
sh0ttky <you@example.com>
2025-04-27 08:30:26 -0500

    fixed culture comp  about page

--- src/components/widgets/Features3.tsx
+++ src/components/widgets/Features3.tsx
@@ -15,9 +15,8 @@ const Features3 = ({
   <WidgetWrapper
     id={id ? id : ''}
     hasBackground={hasBackground}
-    containerClass={`max-w-6xl ${isBeforeContent ? 'md:pb-8 lg:pb-12' : ''} ${
-      isAfterContent ? 'pt-0 md:pt-0 lg:pt-0' : ''
-    }`}
+    containerClass={`max-w-6xl ${isBeforeContent ? 'md:pb-8 lg:pb-12' : ''} ${isAfterContent ? 'pt-0 md:pt-0 lg:pt-0' : ''
+      }`}
   >
     {header && <Headline header={header} titleClass="text-4xl md:text-5xl" />}
     <ItemGrid
@@ -25,8 +24,8 @@ const Features3 = ({
       items={items}
       columns={columns}
       defaultColumns={3}
-      panelClass="card h-full relative flex flex-col text-center p-8"
-      iconClass="h-12 w-12 flex justify-center items-center rounded-md text-white bg-foreground mb-6 p-2 md:p-3 mx-auto"
+      panelClass="card h-full relative flex flex-col text-center border bg-gray-100 hover:shadow-lg p-8"
+      iconClass="h-12 w-12 flex justify-center items-center rounded-md text-white bg-blue-800 mb-6 p-2 md:p-3 mx-auto"
       titleClass="mb-3 text-xl font-bold"
       descriptionClass="text-gray-600 dark:text-slate-400"
     />
